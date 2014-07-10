763d5112a66ceb7dd001b52f258f46799809343d
Matthew Wastrodowski <towski@gmail.com>
2014-07-10 16:19:58 -0700

    swap x and y for some reason, fixes bug

--- godo.go
+++ godo.go
@@ -2,31 +2,18 @@ package main
 
 import(
     "github.com/tncardoso/gocurses"
-    "math/rand"
+//    "math/rand"
     "time"
     "fmt"
     "runtime"
     "os"
     "github.com/towski/Golang-AStar/utils"
+    "sync"
 )
 
 var disp int
 var frames int
 //var wg *sync.WaitGroup
-
-func move(x int, y int) {
-var temp int
-    gocurses.Mvaddstr(y, x, " ")
-    temp = rand.Intn(3)-1
-    x += temp
-    temp = rand.Intn(3)-1
-    y += temp
-    gocurses.Mvaddstr(y, x, "d")
-    gocurses.Refresh()
-    if(disp != 1){
- //       wg.Done()
-    }
-}
 
 type Icon struct {
     x int
@@ -38,7 +25,8 @@ type Icon struct {
 
 func main() {
     frames = 0
-    messages := make(chan Icon)
+    messages := make(chan Icon, 20)
+    var log string
     fmt.Print(time.Second)
     runtime.GOMAXPROCS(4)
     gocurses.Initscr()
@@ -48,10 +36,10 @@ func main() {
     gocurses.CursSet(0)
     gocurses.Stdscr.Keypad(true)
     var scene utils.Scene
-    scene.InitScene(70, 100)
+    scene.InitScene(80, 40)
     scene.AddWalls(20)
     utils.InitAstar(&scene)
-
+    var mutex = &sync.Mutex{}
 
     for j := 0; j < 1; j++ {
         go func() { 
@@ -59,14 +47,14 @@ func main() {
             var started = 0
             var finalPoint *utils.Point
             icon = Icon{}
-            icon.x = 50//rand.Intn(50)
-            icon.y = 50//rand.Intn(50)
+            icon.x = 20//rand.Intn(50)
+            icon.y = 30//rand.Intn(50)
             messages <- icon
             for {
                 if started == 0 {
                     started = 1
-                    scene.Data[3][3] = 'A'
-                    scene.Data[5][5] = 'B'
+                    utils.SetOrig(&scene, 30, 20)
+                    utils.SetDest(&scene, 3, 3)
                     for {
                         utils.FindPath(&scene)
                         //time.Sleep(50 * time.Millisecond)
@@ -77,51 +65,84 @@ func main() {
                     icon.old_x = icon.x
                     icon.old_y = icon.y
                     finalPoint = &utils.FinalPoint
+                    for {
+                        if finalPoint.Parent == nil {
+                            break
+                        } else {
+                            finalPoint = finalPoint.Parent
+            log += fmt.Sprintf("Parent \n")
+                    if finalPoint.Child != nil {
+            log += fmt.Sprintf("Has child \n")
+            }
+                        }
+                    }
                     gocurses.End()
-                    if finalPoint.Parent == nil {
-                    fmt.Println("WHA")
+                    if finalPoint.Child == nil {
+            log += fmt.Sprintf("No Child \n")
                     } else {
-                        icon.x = finalPoint.Parent.X
-                        icon.y = finalPoint.Parent.Y
-                        finalPoint = finalPoint.Parent
+            log += fmt.Sprintf("Child \n")
+                        icon.x = finalPoint.Child.Y
+                        icon.y = finalPoint.Child.X
+                        finalPoint = finalPoint.Child
                         messages <- icon
-                        time.Sleep(1000 * time.Millisecond)
+                        time.Sleep(300 * time.Millisecond)
                    }
                 } else {
-                    icon.old_x = icon.x
-                    icon.old_y = icon.y
-                        icon.x = finalPoint.Parent.X
-                        icon.y = finalPoint.Parent.Y
-                        finalPoint = finalPoint.Parent
+                    if finalPoint.Child != nil {
+                        icon.old_x = icon.x
+                        icon.old_y = icon.y
+                        icon.x = finalPoint.Child.Y
+                        icon.y = finalPoint.Child.X
+                        finalPoint = finalPoint.Child
                         messages <- icon
+                        time.Sleep(300 * time.Millisecond)
+                    } else {
                         time.Sleep(1000 * time.Millisecond)
+                    }
                 }
             }
         }()
     }
-                    //icon.old_x = x
-                    //icon.old_y = y
-                    //temp = rand.Intn(3)-1
-                    //temp = rand.Intn(3)-1
-                    //x += temp
-                    //y += temp
-                    //icon.x = x
-                    //icon.y = y
 
     go func() { 
+            var draw_icon Icon
         for {
-            icon := <-messages
-            gocurses.Mvaddstr(icon.old_y, icon.old_x, " ")
-            gocurses.Mvaddstr(icon.y, icon.x, "d")
+            draw_icon = <-messages
+            mutex.Lock()
+            if draw_icon.old_y != 0 && draw_icon.old_x != 0 {
+                gocurses.Mvaddstr(draw_icon.old_y, draw_icon.old_x, " ")
+            }
+            gocurses.Mvaddstr(draw_icon.y, draw_icon.x, "d")
+            //log += fmt.Sprintf("Drawing d %d, %d\n", draw_icon.x, draw_icon.y)
+            mutex.Unlock()
             gocurses.Refresh()
             frames += 1
+        }
+    }()
+
+    go func() { 
+        var wall_icon Icon
+        wall_icon = Icon{}
+        for i := 0; i < scene.Rows; i++ {
+            for j := 0; j < scene.Cols; j++ {
+                if scene.Data[i][j] == '#' {
+            mutex.Lock()
+             //       log += fmt.Sprintf("Drawing wall %d, %d\n", i, j)
+            mutex.Unlock()
+                    wall_icon.x = j
+                    wall_icon.y = i
+                    messages <- wall_icon
+                }
+            }
         }
     }()
 
     gocurses.Attron(gocurses.A_BOLD)
     disp = 1
     disp = gocurses.Getch()
-     gocurses.End()
+    gocurses.End()
+    fmt.Println(log)
     fmt.Println(frames)
+    scene.Draw()
     os.Exit(0)
 }
